8101b8390279a01ae19d3e3573a85a5ffde42cdb
shunkosa <shun.kosaka.102@gmail.com>
2020-03-31 17:08:29 +0900

    Extract functions

---
+++ src/style/text.ts
@@ -0,0 +1,11 @@
+export const th = text => {
+    return { text, style: 'bold' };
+};
+
+export const h2 = text => {
+    return { text, style: 'h2', margin: [0, 10] };
+};
+
+export const h3 = text => {
+    return { text, style: 'h3', margin: [0, 10] };
+};
