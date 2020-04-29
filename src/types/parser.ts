91d7ff7d08a2a0666d149a91d1c6219e985a6e4b
shunkosa <shun.kosaka.102@gmail.com>
2020-04-29 18:32:04 +0900

    Update types

---
+++ src/types/parser.ts
@@ -0,0 +1,59 @@
+export interface ReadableProcess {
+    processType: string;
+    name: string;
+    label: string;
+    description?: string;
+    triggerType?: string; // only in trigger-based process
+    objectType: string;
+    eventType?: string; // only in platform event process
+    eventMatchingConditions?: Array<ReadableCondition>; // only in platform event process
+    actionGroups: Array<ReadableActionGroup>;
+}
+
+export interface ReadableActionGroup {
+    decision: ReadableDecision;
+    actions?: Array<ReadableActionItem>;
+    scheduledActionSections?: Array<ReadableScheduledActionSection>;
+    evaluatesNext?: boolean;
+}
+
+export interface ReadableDecision {
+    label: string;
+    criteria: string;
+    conditionLogic: string;
+    conditions: Array<ReadableCondition>;
+    formulaExpression?: string;
+}
+
+export interface ReadableCondition {
+    field: string;
+    operator: string;
+    type: string;
+    value: string;
+}
+
+export interface ReadableActionItem {
+    label: string;
+    type: string;
+    detail?: Array<any>;
+    conditions?: Array<ReadableActionItemParameter>;
+    params?: Array<ReadableActionItemParameter>;
+}
+
+export interface ReadableActionItemParameter {
+    field: string;
+    type: string;
+    value: string;
+}
+
+export interface ReadableScheduledActionSection {
+    summary: ReadableWaitEventSummary;
+    actions?: Array<ReadableActionItem>;
+}
+
+export interface ReadableWaitEventSummary {
+    offset: number;
+    unit: string;
+    isAfter: boolean;
+    field?: string;
+}
