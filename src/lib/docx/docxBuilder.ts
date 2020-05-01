aa2812503ffd4b0f815c18ef178f791b6b30070a
shunkosa <shun.kosaka.102@gmail.com>
2020-05-02 00:06:36 +0900

    Add style files

--- src/lib/docx/docxBuilder.ts
+++ src/lib/docx/docxBuilder.ts
@@ -1,14 +1,20 @@
 import * as docx from 'docx';
+import * as path from 'path';
+import * as fs from 'fs';
 import { ReadableProcess } from '../../types/parser';
 import DocxProcessFormatter from './process/docxProcessFormatter';
 
 export default function buildDocxContent(flow: ReadableProcess, locale: string): docx.File {
+    const jaStyles = fs.readFileSync(path.resolve(__dirname, '../../assets/fonts/styles.ja.xml'), 'utf-8');
+    const enStyles = fs.readFileSync(path.resolve(__dirname, '../../assets/fonts/styles.en.xml'), 'utf-8');
     const content = [];
     const dpf = new DocxProcessFormatter(flow, locale);
     content.push(...dpf.prepareHeader());
     content.push(...dpf.prepareStartCondition());
     content.push(...dpf.prepareActionGroups());
-    const doc = new docx.Document();
+    const doc = new docx.Document({
+        externalStyles: locale === 'ja' ? jaStyles : enStyles,
+    });
     doc.addSection({
         children: content,
     });
