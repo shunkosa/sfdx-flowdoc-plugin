9b23467815135b6c2bc2509ac79b562a8f92e4d9
shunkosa <shun.kosaka.102@gmail.com>
2020-05-23 12:29:48 +0900

    Fix

--- src/lib/util/flowUtils.ts
+++ src/lib/util/flowUtils.ts
@@ -4,10 +4,7 @@ export const SUPPORTED_PROCESS = ['Workflow', 'CustomEvent', 'InvocableProcess']
 export const SUPPORTED_FLOW = ['AutoLaunchedFlow'];
 
 export function isSupported(flow: Flow) {
-    return (
-        isProcess(flow) ||
-        (SUPPORTED_FLOW.includes(this.flow.processType) && this.flow.start.recordTriggerType !== undefined)
-    );
+    return isProcess(flow) || (SUPPORTED_FLOW.includes(flow.processType) && flow.start.recordTriggerType !== undefined);
 }
 
 export function isProcess(flow: Flow) {
